d46ae29a120f797d6a2f5ba1363b110d6aebccd2
OriginalUuser <originaluuser@gmail.com>
2026-01-28 15:32:18 +0800

    Added scripts to download csv files and convert to sqlite db

---
+++ scripts/dataset_download.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Pull kaggle dataset from kaggle hub
+curl -L -o "./database_vol/kaggle_dataset.zip" --create-dirs https://www.kaggle.com/api/v1/datasets/download/ogbuokiriblessing/sensor-based-aquaponics-fish-pond-datasets
+unzip "./database_vol/kaggle_dataset.zip" -d "./database_vol/kaggle_dataset"
+rm ./database_vol/kaggle_dataset.zip
+
+# Convert .csv files into Sqlite3 .db file
+index=0
+for CSV_FILE in "./database_vol/kaggle_dataset"/* ; do
+    if [[ -f "$CSV_FILE" && "$CSV_FILE" == *.csv ]]; then
+        echo "$CSV_FILE"
+        TABLE_NAME="iot_pond_$index"
+        sqlite3 "./database_vol/sensor_readings.db" ".mode csv" ".import $CSV_FILE $TABLE_NAME" ".exit"
+        ((index++))
+    fi
+done
