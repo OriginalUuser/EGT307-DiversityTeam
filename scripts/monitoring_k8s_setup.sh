f1099e0f9c5fc026f1c721c8bf07bf770569658e
OriginalUuser <originaluuser@gmail.com>
2026-02-04 17:39:08 +0800

    Added scripts to startup monitoring deployment and overall script to setup the k8s cluster

---
+++ scripts/monitoring_k8s_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check the status of minikube
+if minikube status | grep -q "host: Running"; then
+    kubectl apply -f ./k8s/monitoring/monitor-config.yaml
+    kubectl apply -f ./k8s/monitoring/monitor-storage.yaml
+    kubectl apply -f ./k8s/monitoring/monitor-deployment.yaml
+else
+    echo "Minikube is not running. Aborting script execution."
+    exit 1
+fi
