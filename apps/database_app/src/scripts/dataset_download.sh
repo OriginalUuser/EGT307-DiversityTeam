482da480ea1ecec256176bce28dd889111f592ea
6zekiel <6zekiel@gmail.com>
2026-02-15 11:46:38 +0800

    Created job to insert data to cluster instance of postgres + Configured credentials, configmap + Gave permissions to admin user to create db

---
+++ apps/database_app/src/scripts/dataset_download.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Pull kaggle dataset from kaggle hub
+curl -L -o "./data/kaggle_dataset.zip" --create-dirs https://www.kaggle.com/api/v1/datasets/download/ogbuokiriblessing/sensor-based-aquaponics-fish-pond-datasets
+unzip -o "./data/kaggle_dataset.zip" -d "./data/kaggle_dataset_raw"
+rm ./data/kaggle_dataset.zip
+
+# # Clean dataset tables to match each other
+mkdir ./data/kaggle_dataset_clean
+# sed -i "s/Jan-00/12.45/g" "./data/kaggle_dataset_raw/IoTPond4.csv"
+python ./scripts/python_helpers/raw_dataset_cleaning.py
+
+# Upload data to the database
+python ./scripts/python_helpers/database_csv_upload.py
---
+++ apps/database_app/src/scripts/monitoring_k8s_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check the status of minikube
+if minikube status | grep -q "host: Running"; then
+    kubectl apply -f ./k8s/monitoring/monitor-namespace.yaml
+    kubectl apply -f ./k8s/monitoring/monitor-dbcreds.yaml
+    kubectl apply -f ./k8s/monitoring/monitor-config.yaml
+    kubectl apply -f ./k8s/monitoring/monitor-storage.yaml
+    kubectl apply -f ./k8s/monitoring/monitor-deployment.yaml
+else
+    echo "Minikube is not running. Aborting script execution."
+    exit 1
+fi
