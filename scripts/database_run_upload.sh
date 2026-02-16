a50a590ff98151b69e306682988da838352a0d8f
OriginalUuser <originaluuser@gmail.com>
2026-02-16 14:50:37 +0800

    Altered original database_download to run zhexiang's job instead

---
+++ scripts/database_run_upload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check the status of minikube
+if minikube status | grep -q "host: Running"; then
+    # Wait for database deployment to be ready
+    kubectl wait --for=condition=Ready cluster/sensor-db-ha --namespace=database-ns --timeout=120s
+
+    # Run the data upload job
+    kubectl apply -f kubectl apply -f ./k8s/database/postgres-data-loader.yaml
+else
+    echo "Minikube is not running. Aborting script execution."
+    exit 1
+fi
