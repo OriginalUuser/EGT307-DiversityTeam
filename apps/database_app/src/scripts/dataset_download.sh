181543c159461a038ff043b0dbd244fbf94d7a1e
OriginalUuser <originaluuser@gmail.com>
2026-02-17 23:04:18 +0800

    Fixed dataset upload job

--- apps/database_app/src/scripts/dataset_download.sh
+++ apps/database_app/src/scripts/dataset_download.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Pull kaggle dataset from kaggle hub
-curl -L -o "./data/kaggle_dataset.zip" --create-dirs https://www.kaggle.com/api/v1/datasets/download/ogbuokiriblessing/sensor-based-aquaponics-fish-pond-datasets
-unzip -o "./data/kaggle_dataset.zip" -d "./data/kaggle_dataset_raw"
-rm ./data/kaggle_dataset.zip
+curl -L -o "/app/data/kaggle_dataset.zip" --create-dirs https://www.kaggle.com/api/v1/datasets/download/ogbuokiriblessing/sensor-based-aquaponics-fish-pond-datasets
+unzip -o "/app/data/kaggle_dataset.zip" -d "./data/kaggle_dataset_raw"
+rm /app/data/kaggle_dataset.zip
 
 # # Clean dataset tables to match each other
-mkdir ./data/kaggle_dataset_clean
-# sed -i "s/Jan-00/12.45/g" "./data/kaggle_dataset_raw/IoTPond4.csv"
-python ./scripts/python_helpers/raw_dataset_cleaning.py
+mkdir /app/data/kaggle_dataset_clean
+python /app/scripts/python_helpers/raw_dataset_cleaning.py
 
 # Upload data to the database
-python ./scripts/python_helpers/database_csv_upload.py
+python /app/scripts/python_helpers/database_csv_upload.py
--- scripts/database_run_upload.sh
+++ scripts/database_run_upload.sh
@@ -6,7 +6,6 @@ if minikube status | grep -q "host: Running"; then
     kubectl wait --for=condition=Ready cluster/sensor-db-ha --namespace=database-ns --timeout=120s
 
     # Run the data upload job
-    kubectl apply -f ./k8s/database/sensor-database/postgres-credentials.yaml
     kubectl apply -f ./k8s/database/sensor-database/postgres-job-configmap.yaml
     kubectl apply -f ./k8s/database/sensor-database/postgres-data-loader.yaml
 else
