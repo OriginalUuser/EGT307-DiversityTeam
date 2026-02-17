91f0736aa8082879b876d2a18d6f49a49e978df4
OriginalUuser <originaluuser@gmail.com>
2026-02-18 01:01:34 +0800

    Added cronjob to the monitoring setup

--- scripts/monitoring_k8s_setup.sh
+++ scripts/monitoring_k8s_setup.sh
@@ -2,11 +2,18 @@
 
 # Check the status of minikube
 if minikube status | grep -q "host: Running"; then
+    # Setup the namespace
     kubectl apply -f ./k8s/monitoring/monitor-namespace.yaml
+
+    # Monitoring Application
     kubectl apply -f ./k8s/monitoring/monitor-dbcreds.yaml
     kubectl apply -f ./k8s/monitoring/monitor-config.yaml
     kubectl apply -f ./k8s/monitoring/monitor-storage.yaml
     kubectl apply -f ./k8s/monitoring/monitor-deployment.yaml
+
+    # Monitoring CronJob
+    kubectl apply -f ./k8s/monitoring/monitor-serviceacc.yaml
+    kubectl apply -f ./k8s/monitoring/monitor-cronjob.yaml
 else
     echo "Minikube is not running. Aborting script execution."
     exit 1
