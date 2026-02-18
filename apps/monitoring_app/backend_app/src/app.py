3322dfcdd13e2d7a104bcad59c9d287e569feb4b
OriginalUuser <originaluuser@gmail.com>
2026-02-18 12:54:43 +0800

    Rewrote monitoring backend code to use procrastinate

--- apps/monitoring_app/backend_app/src/app.py
+++ apps/monitoring_app/backend_app/src/app.py
@@ -1,33 +1,37 @@
 import os
 import time
 import logging
+import requests
 
 import pandas as pd
 from sqlalchemy import create_engine
 from .reporting import generate_report, is_data_drift
 from evidently.ui.workspace import RemoteWorkspace
 
-from fastapi import FastAPI, status
-from pydantic import BaseModel
-from requests.exceptions import ConnectionError
+import procrastinate
+from procrastinate.contrib.aiopg import AiopgConnector
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-app = FastAPI()
-
 # Accessing the database
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
 POSTGRES_PASS = os.getenv("POSTGRES_PASS")
-DATABASE_DNS = os.getenv("DATABASE_DNS")
+DATABASE_HOST = os.getenv("DATABASE_HOST")
 DATABASE_PORT = os.getenv("DATABASE_PORT")
+DATABASE_URI = f"postgresql://{DATABASE_USER}:{POSTGRES_PASS}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
-DB_NAME     = "sensor-db"
-USER        = "admin"
-PASSWORD    = POSTGRES_PASS
-HOST        = DATABASE_DNS
-PORT        = DATABASE_PORT
-engine = create_engine(f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}')
+engine = create_engine(DATABASE_URI)
 logger.debug("Engine created successfully")
+
+# Get training url
+TRAINING_PORT = os.getenv("TRAINING_PORT")
+TRAINING_DNS = os.getenv("TRAINING_DNS")
+TRAINING_URL = f"http://{TRAINING_DNS}:{TRAINING_PORT}"
+
+# Setup procrastinate
+app = procrastinate.App(connector=AiopgConnector(dsn=DATABASE_URI)) 
 
 # Connect to the remote workspace for updating the monitoring dashboards
 WORKSPACE_URL = os.getenv("WORKSPACE_URL")
@@ -46,25 +50,29 @@ logger.debug("Workspace project connected successfully")
 project = ws.search_project("Aquaponics Monitoring")
 if isinstance(project, list): project = project[0]
 
-class Evaluate(BaseModel):
-    table_name          : str
-    columns_to_check    : list[str] = ["temperature", "turbidity", "dissolved_oxygen", "ph", "ammonia", "nitrate", "population", "fish_length", "fish_weight"]
-    report_range        : int       = 10000
-
-@app.post("/", status_code=status.HTTP_202_ACCEPTED)
-async def post_root(payload: Evaluate):
+@app.task(name="generate_report")
+async def generate_report(
+    batch_id: str,
+    table_name: str,
+    columns_to_check: list[str],
+    report_range: int
+):
     # Get the most recent {report_range} rows
-    sql_query = f"SELECT * FROM {payload.table_name} ORDER BY created_at DESC FETCH FIRST {payload.report_range} ROWS ONLY;"
+    sql_query = f"SELECT * FROM {table_name} ORDER BY created_at DESC FETCH FIRST {report_range} ROWS ONLY;"
     df = pd.read_sql(sql_query, engine)
-    df = df.loc[:, payload.columns_to_check]
+    df = df.loc[:, columns_to_check]
     logger.debug("Data has been loaded")
 
+    # If the report range is larger than the dataset, use the dataset's max length instead
+    if report_range > df.shape[0]:
+        report_range = df.shape[0]
+
     # Split extracted data into half for reference data and current data splits
-    if payload.report_range % 2 != 0:
+    if report_range % 2 != 0:
         # Report range must be even
-        report_range = payload.report_range + 1
+        report_range = report_range + 1
     else:
-        report_range = payload.report_range
+        report_range = report_range
 
     halfway_point = int(report_range / 2)
     current = df.iloc[:halfway_point, :]
@@ -74,22 +82,24 @@ async def post_root(payload: Evaluate):
     drift_snapshot, drift_snapshot_dict = generate_report(
         reference_df=reference, 
         current_df=current, 
-        columns=payload.columns_to_check,
-        metadata={"table": payload.table_name}
+        columns=columns_to_check,
+        metadata={"table": table_name}
     )
 
     # Send the report to the dashboard
     ws.add_run(project.id, drift_snapshot)
-    logger.debug("Report generated and added to workspace")
+    logger.info(f"Report Generated for batch: {batch_id}") 
 
     # If model retraining is required, call the retraining pipeline
     if is_data_drift(drift_snapshot_dict):
-        # TODO: Send retraining request to the training pipeline service
         logger.debug("Data drift detected, sending retraining request")
-        pass
+        data_payload = {
+            "tables": [table_name],
+            "rows_limit": halfway_point
+        }
+        response = requests.post(url=TRAINING_URL, json=data_payload)
 
-    return 
-
-@app.get("/", status_code=status.HTTP_200_OK)
-def get_root():
-    return
+        logger.info(f"Response: {response.status_code}") 
+        logger.info(f"Response: {response.text}") 
+    else:
+        logger.info(f"No data drift detected")
