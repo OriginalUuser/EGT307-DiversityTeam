5686816390d68deec1157aa066d3c1fa3b7252b6
YohanMatt2 <yohanmatt2@gmail.com>
2026-02-20 02:33:11 +0800

    created cronjob for scheduled model inference

--- scripts/dashboard_k8s_setup.sh
+++ scripts/dashboard_k8s_setup.sh
@@ -7,8 +7,10 @@ DASHBOARD_K8S_PATH="$SCRIPT_DIR/../k8s/dashboard"
 # Check the status of minikube
 if minikube status | grep -q "host: Running"; then
     kubectl apply -f "${DASHBOARD_K8S_PATH}/dashboard-namespace.yaml"
+    kubectl apply -f "${DASHBOARD_K8S_PATH}/dashboard-storage.yaml"
     kubectl apply -f "${DASHBOARD_K8S_PATH}/dashboard-dbcreds.yaml"
     kubectl apply -f "${DASHBOARD_K8S_PATH}/dashboard-config.yaml"
+    kubectl apply -f "${DASHBOARD_K8S_PATH}/dashboard-cronjob.yaml"
     kubectl apply -f "${DASHBOARD_K8S_PATH}/dashboard-deployment.yaml"
 else
     echo "Minikube is not running. Aborting script execution."
